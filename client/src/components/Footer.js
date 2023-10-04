da4b56a988c72781ab93cfa073406347e1150eba
nalla venkatesh <venkateshnalla175@gmail.com>
2023-10-04 18:32:01 +0530

    integrated API

--- client/src/components/Footer.js
+++ client/src/components/Footer.js
@@ -2,9 +2,28 @@ import React from 'react'
 
 function Footer() {
   return (
+    <>
     <div>
-      ©️2023
+<footer class=" shadow bg-gray-800">
+    <div class="w-full mx-auto max-w-screen-xl p-4 md:flex md:items-center md:justify-between">
+      <span class="text-sm  sm:text-center text-white">© 2023 <a href="https://flowbite.com/" class="hover:underline">TitleGenius™</a>. All Rights Reserved.
+    </span>
+    <ul class="flex flex-wrap items-center mt-3 text-sm font-medium text-white  sm:mt-0">
+        <li>
+            <a href="#" class="mr-4 hover:underline md:mr-6 ">About</a>
+        </li>
+        <li>
+            <a href="#" class="mr-4 hover:underline md:mr-6">Privacy Policy</a>
+        </li>
+        <li>
+            <a href="#" class="hover:underline">Contact</a>
+        </li>
+    </ul>
     </div>
+</footer>
+
+    </div>
+    </>
   )
 }
 

d6cd7a8b417a26bca7ec31e5351ff6c0ca2cc2b9
tharun <tharunmadishetti1@gmail.com>
2023-10-04 14:48:51 +0530

    added footer

--- client/src/App.js
+++ client/src/App.js
@@ -3,6 +3,7 @@ import Login from './components/Login';
 import Register from './components/Register';
 import { Routes,Route, useNavigate } from 'react-router-dom';
 import NavBar from './components/NavBar';
+import Footer from './components/Footer';
 import axios from 'axios';
 import MyTitles from './components/MyTitles'
 import { loginRoute } from './Utils/APIRoutes';
@@ -60,6 +61,7 @@ function App() {
             <Route exact path='/login' element={<Login user={user} setUser={setUser}/>}/>
             <Route exact path="*" element={<ErrorPage errorCode={404} errorMessage="Page Not Found"/>}/>
         </Routes>
+        <Footer/>
     </div>
   );
 }
